7e9a4774ad476c40ca450f715f2b3d874073f5e4
ap2314 <ap2314@ic.ac.uk>
2016-02-24 15:58:35 +0000

    First attempt at bloodlust
    Signed-off-by: ap2314 <ap2314@ic.ac.uk>

--- my_wol.pl
+++ my_wol.pl
@@ -79,5 +79,85 @@ test_strategy_helper(N, InitN, Strat1, Strat2, TotalMoves, BlueWins, RedWins, Hi
   NewN is N - 1,
   test_strategy_helper(NewN, InitN, Strat1, Strat2, NewTotalMoves, NewBlueWins, NewRedWins, NewHighest, NewLowest, NewTotalTime). 
 
- 
+bloodlust('b', [AliveBlues, AliveReds], [NewAliveBlues, AliveReds], Move) :-
+  bloodlust_move(AliveBlues, AliveReds, Move, 'b'),
+  alter_board(Move, AliveBlues, NewAliveBlues).
+  %format("MOVE BEING PICKED IS: ~w\n", [Move]).
+  
+bloodlust('r', [AliveBlues, AliveReds], [AliveBlues, NewAliveReds], Move) :-
+  bloodlust_move(AliveReds, AliveBlues, Move, 'r'),
+  alter_board(Move, AliveReds, NewAliveReds).
+  %format("MOVE BEING PICKED IS: ~w\n", [Move]).
+
+bloodlust_move(Alive, OtherPlayerAlive, Move, Player) :-
+  
+  % Find all the possible moves given a player colour and a current board state
+  findall([A,B,MA,MB],(member([A,B], Alive),
+                       neighbour_position(A,B,[MA,MB]),
+                       \+member([MA,MB],Alive),
+                       \+member([MA,MB],OtherPlayerAlive)),  
+          PossMoves),
+  
+  (Player ==  'b' ->
+   State = [Alive, OtherPlayerAlive]
+  ;
+   State = [OtherPlayerAlive, Alive]
+  ),
+
+  % Given a list of possible moves, run move and crank for each, return the lowest
+  bloodlust_move_helper(PossMoves, 65, Move, State, Player).
+
+bloodlust_move_helper([X], Min, Move, State, Player) :-
+  move_and_crank(X, State, [NewBlues, NewReds]),
+  
+  % Check the length of the opposing players alive list, if smaller than min, return as move
+  (Player == 'b' ->
+    length(NewReds, RLen),
+    % format("Length of NewReds is: ~w\n", [RLen]),
+    (RLen < Min ->
+      NewMin is RLen,
+      Move = X
+    ;
+      NewMin is Min
+    )
+  ;
+    length(NewBlues, BLen),
+    (BLen < Min ->
+      NewMin is BLen,
+      Move = X
+    ;
+      NewMin is Min
+  )).
+  
+
+bloodlust_move_helper([X|Xs], Min, Move, State, Player) :-
+  move_and_crank(X, State, [NewBlues, NewReds]),
+  
+  % Check the length of the opposing players alive list, if smaller than min, return as move
+  (Player == 'b' ->
+    length(NewReds, RLen),
+    (RLen < Min ->
+      NewMin is RLen,
+      Move = X
+    ;
+      NewMin is Min
+    )
+  ;
+    length(NewBlues, BLen),
+    (BLen < Min ->
+      NewMin is BLen,
+      Move = X
+    ;
+      NewMin is Min
+  )),
+
+  bloodlust_move_helper(Xs, NewMin, Move, State, Player).
+
+
+
+move_and_crank(Move, [AliveBlues, AliveReds], [NewAliveBlues, NewAliveReds]) :-
+  alter_board(Move, AliveBlues, IntAliveBlues),
+  alter_board(Move, AliveReds, IntAliveReds),
+  next_generation([IntAliveBlues,IntAliveReds], [NewAliveBlues,NewAliveReds]).
+
 
