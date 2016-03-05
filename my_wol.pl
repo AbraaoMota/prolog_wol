dd6f19d589e08524c523dabe88093414bf6d3d14
ap2314 <ap2314@ic.ac.uk>
2016-03-05 22:30:51 +0000

    Commented file, finished report
    Signed-off-by: ap2314 <ap2314@ic.ac.uk>

--- my_wol.pl
+++ my_wol.pl
@@ -1,10 +1,23 @@
 %prolog
 
 
+% Wrapper for function called in war_of_life
 test_strategy(N, Strat1, Strat2) :-
   test_strategy_helper(N, N, Strat1, Strat2, 0, 0, 0, 0, 250, 0).
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Base case of test_strategy helper (1 game)
+%
+% 
+% Args: (1)    - Variable number of games used to recurse on
+%       (2)    - Initial Number of games played (used for statistics)
+%       (3, 4) - Player 1 and 2 Strategies
+%       (5)    - Counter keeping total moves in all the games
+%       (6, 7) - Number of BlueWins and RedWins
+%       (8, 9) - Highest/ Lowest number of moves in any game ran in this recursion
+%       (10)   - Total time spent in games, measured in ms
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 test_strategy_helper(1, InitN, Strat1, Strat2, TotalMoves, BlueWins, RedWins, Highest, Lowest, TimeInGame) :-
 
   % Grab the game time whilst playing the game
@@ -64,7 +77,14 @@ test_strategy_helper(1, InitN, Strat1, Strat2, TotalMoves, BlueWins, RedWins, Hi
   format("Average Time per game: ~w\n\n", [AverageTime]).
 
 
-% Recursive case for test_strategy_helper (>1 games to play)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Recursive case of test_strategy helper
+%
+% Args: See above in base case
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 test_strategy_helper(N, InitN, Strat1, Strat2, TotalMoves, BlueWins, RedWins, Highest, Lowest, TimeInGame) :-
   
   % Grab game time statistics and play the game
@@ -111,11 +131,22 @@ test_strategy_helper(N, InitN, Strat1, Strat2, TotalMoves, BlueWins, RedWins, Hi
 
 
 %%%%%%%%%%%%%%%%%%%%%%%
-% Utilities           %
+%
+% Utilities           
+% 
 %%%%%%%%%%%%%%%%%%%%%%%
 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Performs a move and runs conways crank on it,
-% Returns a new state when given a move, a state and a player
+%
+% Args: (1) - Move given to perform and analyse
+%       (2) - A state of [B, R], corresponding to the states defined in war_of_life
+%       (3) - The new state [NB, NR], after having executed move given on state given,
+%             and called conways crank on it
+%       (4) - The player making the move
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 move_and_crank(Move, [AliveBlues, AliveReds], [NewAliveBlues, NewAliveReds], Player) :-
     (Player == 'b' ->
       alter_board(Move, AliveBlues, IntAliveBlues),
@@ -127,15 +158,20 @@ move_and_crank(Move, [AliveBlues, AliveReds], [NewAliveBlues, NewAliveReds], Pla
     next_generation([IntAliveBlues,IntAliveReds], [NewAliveBlues,NewAliveReds]).
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 % Returns the other player given a player
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 other_player('b', 'r').
 other_player('r', 'b').
 
 
-% Find all possible moves for a player with a corresponding set of Alive pieces. Requires
-% also the set of OtherPlayerAlive pieces for the other player. Returns also the 
-% FirstMove of the Moves set, which is useful in being the first suggestedMove in
-% strategies later
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Find all possible moves for a player given his 
+% piece's state and the opponent's state, in
+% that order
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 find_poss_move(Alive, OtherPlayerAlive, Moves) :-
   findall([A,B,MA,MB],(member([A,B], Alive),
                        neighbour_position(A,B,[MA,MB]),
@@ -143,10 +179,25 @@ find_poss_move(Alive, OtherPlayerAlive, Moves) :-
                        \+member([MA,MB],OtherPlayerAlive)),
           Moves).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% bloodlust strategy             %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%         STRATEGIES
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Bloodlust strategy             
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Wrappers for bloodlust depending on what player is called
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 bloodlust('b', [AliveBlues, AliveReds], [NewAliveBlues, AliveReds], Move) :-
   bloodlust_move(AliveBlues, AliveReds, Move, 'b'),
   alter_board(Move, AliveBlues, NewAliveBlues).
@@ -155,6 +206,18 @@ bloodlust('r', [AliveBlues, AliveReds], [AliveBlues, NewAliveReds], Move) :-
   bloodlust_move(AliveReds, AliveBlues, Move, 'r'),
   alter_board(Move, AliveReds, NewAliveReds).
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Wrapper for the move_helper. This method prepares a list of moves 
+% and prepares the state to give to the helper
+%
+% Args: (1) - Current player's alive pieces
+%       (2) - Opponents alive pieces
+%       (3) - Final Move (Returned from below)
+%       (4) - Current player
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 bloodlust_move(Alive, OtherPlayerAlive, Move, Player) :-
 
   % Find all the possible moves given a player colour and a current board state
@@ -172,6 +235,16 @@ bloodlust_move(Alive, OtherPlayerAlive, Move, Player) :-
 
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Run the actual bloodlust strategy
+%
+% Args: (1) - List of moves a player has available to him
+%       (2) - The running minimum to compare against
+%       (3) - Currently suggested Move
+%       (4) - Final move - RETURNED
+%       (5) - State the player is in when they make the move
+%       (6) - Current player
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 bloodlust_move_helper([], _, SuggestedMove, FinalMove, _, _) :-
   FinalMove = SuggestedMove.
 
@@ -206,9 +279,16 @@ bloodlust_move_helper([X|Xs], Min, SuggestedMove, Move, State, Player) :-
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% self_preservation strategy     %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Self_preservation strategy    
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%%%%%%%%%%%%%%
+%
+% Wrappers
+%
+%%%%%%%%%%%%%%%
 self_preservation('b', [AliveBlues, AliveReds], [NewAliveBlues, AliveReds], Move) :-
   self_preservation_move(AliveBlues, AliveReds, Move, 'b'),
   alter_board(Move, AliveBlues, NewAliveBlues).
@@ -217,6 +297,11 @@ self_preservation('r', [AliveBlues, AliveReds], [AliveBlues, NewAliveReds], Move
   self_preservation_move(AliveReds, AliveBlues, Move, 'r'),
   alter_board(Move, AliveReds, NewAliveReds).
 
+%%%%%%%%%%%%%%%
+%
+% Prepare the moveset and states as described in bloodlust
+%
+%%%%%%%%%%%%%%%
 self_preservation_move(Alive, OtherPlayerAlive, Move, Player) :-
 
   % Find all the possible moves given a player colour and a current board state
@@ -232,7 +317,16 @@ self_preservation_move(Alive, OtherPlayerAlive, Move, Player) :-
   % Given a list of possible moves, run move and crank for each, return the lowest
   self_preservation_move_helper(Moves, 0, P1, Move, State, Player).
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Run the actual self_preservation strategy
+%
+% Args: (1) - List of moves a player has available to him
+%       (2) - The running maximum to compare against
+%       (3) - Currently suggested Move
+%       (4) - Final move - RETURNED
+%       (5) - State the player is in when they make the move
+%       (6) - Current player
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 self_preservation_move_helper([], _, SuggestedMove, FinalMove, _, _) :-
   FinalMove = SuggestedMove.
 
@@ -265,11 +359,15 @@ self_preservation_move_helper([X|Xs], Max, SuggestedMove, Move, State, Player) :
   % Recursively call the self_preservation function
   self_preservation_move_helper(Xs, NewMax, NewSuggestedMove, Move, State, Player).
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% land_grab strategy             %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Land Grab strategy    
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%%%%%%%%%%%
+% Wrappers
+%%%%%%%%%%%%
 land_grab('b', [AliveBlues, AliveReds], [NewAliveBlues, AliveReds], Move) :-
   land_grab_move(AliveBlues, AliveReds, Move, 'b'),
   alter_board(Move, AliveBlues, NewAliveBlues).
@@ -278,6 +376,11 @@ land_grab('r', [AliveBlues, AliveReds], [AliveBlues, NewAliveReds], Move) :-
   land_grab_move(AliveReds, AliveBlues, Move, 'r'),
   alter_board(Move, AliveReds, NewAliveReds).
 
+%%%%%%%%%%%
+%
+% Prepare moveset and states
+%
+%%%%%%%%%%%
 land_grab_move(Alive, OtherPlayerAlive, Move, Player) :-
 
   % Find all the possible moves given a player colour and a current board state
@@ -292,7 +395,16 @@ land_grab_move(Alive, OtherPlayerAlive, Move, Player) :-
   % Given a list of possible moves, run move and crank for each, return the lowest
   land_grab_move_helper(Moves, 0, P1, Move, State, Player).
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Run the actual landgrab strategy
+%
+% Args: (1) - List of moves a player has available to him
+%       (2) - The running maximum to compare against
+%       (3) - Currently suggested Move
+%       (4) - Final move - RETURNED
+%       (5) - State the player is in when they make the move
+%       (6) - Current player
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 land_grab_move_helper([], _, SuggestedMove, FinalMove, _, _) :-
   FinalMove = SuggestedMove.
 
@@ -326,10 +438,16 @@ land_grab_move_helper([X|Xs], Max, SuggestedMove, Move, State, Player) :-
   land_grab_move_helper(Xs, NewMax, NewSuggestedMove, Move, State, Player).
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% minimax strategy               %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Minimax strategy    
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%
+% Wrappers
+%%%%%%%%%%%%
 minimax('b', [AliveBlues, AliveReds], [NewAliveBlues, AliveReds], Move) :-
   minimax_move(AliveBlues, AliveReds, Move, 'b'),
   alter_board(Move, AliveBlues, NewAliveBlues).
@@ -338,6 +456,11 @@ minimax('r', [AliveBlues, AliveReds], [AliveBlues, NewAliveReds], Move) :-
   minimax_move(AliveReds, AliveBlues, Move, 'r'),
   alter_board(Move, AliveReds, NewAliveReds).
 
+%%%%%%%%%%%
+%
+% Prepare moveset and states
+%
+%%%%%%%%%%%
 minimax_move(Alive, OtherPlayerAlive, Move, Player) :-
 
   % Find all the possible moves given a player colour and a current board state
@@ -353,6 +476,17 @@ minimax_move(Alive, OtherPlayerAlive, Move, Player) :-
   % Given a list of possible moves, run move and crank for each, return the lowest
   minimax_move_helper(Moves, 0, State, Player, M1, Move).
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Calculates the most damage an enemy move can do to us
+%
+% Args: (1) - Moveset that the enemy has available
+%       (2) - State the enemy has to start from
+%       (3) - Current player (not the enemy himself)
+%       (4) - Currently running minimum, changes on loops
+%       (5) - FinalMinimum (Returned to minimax_move_helper)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 get_enemy_min([], _, _, Min, NewMin) :-
   NewMin = Min. 
 
@@ -362,6 +496,7 @@ get_enemy_min([M1|Moves], State, Player, Min, FinalMin) :-
   length(NewBlues, BLen),
   length(NewReds, RLen),
  
+  % Minimise the initial player's strategy (NOT Maximise ours as if we had land grab)
   (Player == 'b' ->
     (BLen - RLen < Min ->
       NewMin is BLen - RLen           
@@ -375,9 +510,21 @@ get_enemy_min([M1|Moves], State, Player, Min, FinalMin) :-
       NewMin = Min
     )
   ),
+
+  % Recurse
   get_enemy_min(Moves, State, Player, NewMin, FinalMin).
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Run the actual minimax strategy
+%
+% Args: (1) - List of moves a player has available to him
+%       (2) - The running maximum to compare against
+%       (3) - Current state
+%       (4) - Current Player
+%       (5) - Currently Suggested move
+%       (6) - Final move - RETURNED
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 minimax_move_helper([], _, _, _, SuggestedMove, FinalMove) :-
   FinalMove = SuggestedMove.
 
@@ -385,6 +532,8 @@ minimax_move_helper([M1|RemMoves], Max, State, Player, SuggestedMove, FinalMove)
 
   move_and_crank(M1, State, [NewBlues, NewReds], Player),
 
+  % Depending on who the current player is, prepares the state of the enemy
+  % to be called for the find_poss_move function
   (Player == 'r' ->
     Alive = NewBlues,
     OtherPlayerAlive = NewReds
@@ -393,10 +542,13 @@ minimax_move_helper([M1|RemMoves], Max, State, Player, SuggestedMove, FinalMove)
     OtherPlayerAlive = NewBlues
   ),
  
+  % Find the enemy's moveset given we have made move M1
   find_poss_move(Alive, OtherPlayerAlive, Moves),
 
+  % Get the most damaging enemy move from this set
   get_enemy_min(Moves, [NewBlues, NewReds], Player, 65, Min),
 
+  % If the new worst from this move is better than our previous worst, pick this new worst
   (Min > Max ->
     NewSuggestedMove = M1,
     NewMax = Min
@@ -405,5 +557,6 @@ minimax_move_helper([M1|RemMoves], Max, State, Player, SuggestedMove, FinalMove)
     NewMax = Max
   ),
 
+  % Recurse
   minimax_move_helper(RemMoves, NewMax, State, Player, NewSuggestedMove, FinalMove).
 
